19a4006e97b08f4943e46830be6c39349fb0ea76
maksim.samorukov <maxim.samorukov@gmail.com>
2026-02-18 16:34:25 +0300

    feat: added state

---
+++ src/hooks/useDevices.tsx
@@ -0,0 +1,72 @@
+import { useCallback, useEffect, useState } from "react";
+import { api } from "../api";
+import type { Device, Place } from "../types";
+
+export const useDevices = () => {
+  const [devices, setDevices] = useState<Device[]>([]);
+  const [device, setDevice] = useState<Device | null>(null);
+  const [place, setPlace] = useState<Place | null>(null);
+  const [balanceUpdateInProgress, setBalanceUpdateInProgress] =
+    useState<boolean>(false);
+
+  useEffect(() => {
+    if (!device?.id) {
+      api.getDevices().then((data) => {
+        setDevices(data);
+      });
+    }
+  }, [device?.id]);
+
+  const selectDevice = useCallback((id: number) => {
+    api
+      .getDeviceById(id.toString())
+      .then((data) => {
+        setDevice(data);
+      })
+      .catch(() => {
+        setDevice(null);
+      });
+  }, []);
+  const getBackToDeviceList = useCallback(() => {
+    setDevice(null);
+    setPlace(null);
+  }, []);
+
+  const updateBalance = useCallback(
+    (delta: number, deviceId: number, placeId: number) => {
+      setBalanceUpdateInProgress(true);
+      api
+        .updateBalance(deviceId.toString(), placeId.toString(), { delta })
+        .then((data) => {
+          if (data) {
+            setPlace(data);
+            setDevice((d) => {
+              if (!d) return d;
+              const updatedPlaces = (d?.places || []).map((i) => {
+                if (i.place === placeId) {
+                  return data;
+                }
+                return i;
+              });
+              return {
+                ...d,
+                places: updatedPlaces,
+              };
+            });
+          }
+        })
+        .finally(() => {
+          setBalanceUpdateInProgress(false);
+        });
+    },
+    [place],
+  );
+  return {
+    device,
+    devices,
+    selectDevice,
+    getBackToDeviceList,
+    balanceUpdateInProgress,
+    updateBalance,
+  };
+};
